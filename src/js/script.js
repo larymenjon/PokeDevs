b533342c488d34b8fee89a7a92d4849b222e14af
Laryssa Aguiar Menjon <laaryag@gmail.com>
2024-10-23 13:35:38 -0300

    Mudanças

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,31 @@ document.addEventListener("DOMContentLoaded", () => {
   // Seleciona todos os elementos da lista de pokedevs e os cartões
   const pokedevList = document.querySelectorAll(".pokedev");
   const pokedevCards = document.querySelectorAll(".cartao-pokedev");
+  const radioList = document.getElementById("radio-list");
+
+  // Função para buscar rádios
+  const fetchRadios = async () => {
+    const response = await fetch(
+      "https://de1.api.radio-browser.info/json/stations"
+    );
+    const radios = await response.json();
+
+    // Limpa a lista antes de adicionar novas rádios
+    radioList.innerHTML = "";
+
+    // Adiciona rádios à lista
+    radios.forEach((radio) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = radio.name;
+      listItem.addEventListener("click", () => {
+        window.open(radio.url, "_blank"); // Abre a rádio em uma nova aba
+      });
+      radioList.appendChild(listItem);
+    });
+  };
+
+  // Chama a função para buscar rádios
+  fetchRadios();
 
   pokedevList.forEach((pokedev) => {
     pokedev.addEventListener("click", () => {
